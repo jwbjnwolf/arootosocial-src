3ea23057db7f183974a63e14a94e2a9fc798b5e1
jwbjnwolf <59905825+jwbjnwolf@users.noreply.github.com>
2024-09-26 12:26:23 +0100

    Add assets build commands to convenience build scripts

--- scripts/build_amd64_linux.sh
+++ scripts/build_amd64_linux.sh
@@ -4,4 +4,11 @@ export GOOS=linux
 export GOARCH=amd64
 echo $GOOS $GOARCH
 
+echo "Building binary..."
 sh ./scripts/build.sh
+
+### Building web assets dist
+echo "Building web/assets/dist..."
+yarn --cwd ./web/source install && yarn --cwd ./web/source ts-patch install
+yarn --cwd ./web/source build
+
