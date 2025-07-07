f074554459c87dc2b08208e75d38faec126b1b4d
jwbjnwolf <59905825+jwbjnwolf@users.noreply.github.com>
2025-07-07 05:19:23 +0100

    attempt to have 50 results for post search instead of 20
    Still get 20 results for post searching in Tusky, when the keyword I'm searching is in far greater amount of posts.
    
    the `apiutil.LimitKey` setting here seems most and only relevant code with the value for the amount the client is allowed to return. Am I missing something else?
    
    Not even sure if the change in the swagger.yaml means anything here.
    
    Taking note from commit https://github.com/jwbjnwolf/arootosocial-src/commit/831ae09f8bab04af854243421047371339c3e190 of which the partial text search was added.

--- internal/api/client/search/searchget.go
+++ internal/api/client/search/searchget.go
@@ -203,7 +203,7 @@ func (m *Module) SearchGETHandler(c *gin.Context) {
 		return
 	}
 
-	limit, errWithCode := apiutil.ParseLimit(c.Query(apiutil.LimitKey), 20, 40, 1)
+	limit, errWithCode := apiutil.ParseLimit(c.Query(apiutil.LimitKey), 50, 100, 1)
 	if errWithCode != nil {
 		apiutil.ErrorHandler(c, errWithCode, m.processor.InstanceGetV1)
 		return
